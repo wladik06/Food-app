b188e95a55bfcb6a885ccb27fde27e93441982ed
Vladimir <57994892+wladik06@users.noreply.github.com>
2022-06-06 21:35:30 +0200

    ULP02 - Created tests

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_login()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+
+    public function test_duplication()
+    {
+        $user1 = User::make([
+            'name' => 'User1',
+            'email' => 'user1@gmail.com'
+        ]);
+        $user2 = User::make([
+            'name' => 'User2',
+            'email' => 'user2@gmail.com'
+        ]);
+
+        $this->assertTrue($user1 -> name != $user2 -> name);
+    }
+
+    public function test_delete()
+    {
+        $user = User::factory() -> count(1) -> make();
+        $user = User::first();
+        if($user) {
+            $user -> delete();
+        }
+
+        $this -> assertTrue(true);
+    }
+
+    public function test_store_user()
+    {
+        $response = $this -> post('/register', [
+            'name' => 'User1',
+            'email' => 'user1@gmail.com',
+            'password' => 'user1234'
+        ]);
+
+        $response -> assertRedirect('/login');
+    }
+
+
+}
