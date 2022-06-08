b5be68404f51955e20839b56babf63228eaaeef4
Vladimir <57994892+wladik06@users.noreply.github.com>
2022-06-09 01:59:04 +0200

    ULP04 - Laravel dusk - browser testing

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,22 +2,33 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class LoginTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testHome()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                     ->assertSee('Collection');
+        });
+    }
+
+    public function testLogin()
+    {
+        $user = User::factory()->create([
+            'email' => 'test1@test.com',
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user -> email)
+                ->type('password', 'password')
+                ->press('Login')
+                ->assertPathIs('/');
         });
     }
 }
